169050589071556b2823ea050ef12ef27f0cb0e1
_hayas <hajcus10@seznam.cz>
2024-11-29 15:31:06 +0100

    [EMAIL] - Java mail sender demo

--- src/main/java/dev/playground/JavaPlaygroundApplication.java
+++ src/main/java/dev/playground/JavaPlaygroundApplication.java
@@ -1,13 +1,29 @@
 package dev.playground;
 
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class JavaPlaygroundApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaPlaygroundApplication.class, args);
+	}
+
+	@Autowired
+	private EmailService emailService;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException {
+		emailService.sendEmailWithAttachment(
+				"spring.email.to@gmail.com",
+				"Sending you email with attachment",
+				"This email has attachment",
+				"C:\\Users\\public\\picture.jpeg");
 	}
 
 }
