0da40272d7e83202f98327455da9d1be91149515
_hayas <hajcus10@seznam.cz>
2025-02-19 19:22:11 +0100

    [gRPC] - file upload using grpc client-streaming

---
+++ shared/src/main/java/dev/playground/Constants.java
@@ -0,0 +1,11 @@
+package dev.playground;
+
+import io.grpc.Context;
+import io.grpc.Metadata;
+
+public class Constants {
+
+    public static final  Metadata.Key<byte[]> fileMetadataKey = Metadata.Key.of("file-meta-bin", Metadata.BINARY_BYTE_MARSHALLER);
+    public static final Context.Key<FileMetadata> fileMetaContext = Context.key("file-meta");
+
+}
