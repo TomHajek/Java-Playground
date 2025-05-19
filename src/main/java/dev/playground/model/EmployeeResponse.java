b91361d744cbe5cf7f476db90c98d7ce8d15e014
_hayas <hajcus10@seznam.cz>
2025-05-19 15:02:09 +0200

    [S1] - calling S2 from S1 using RestTemplate

--- src/main/java/dev/playground/model/EmployeeResponse.java
+++ src/main/java/dev/playground/model/EmployeeResponse.java
@@ -6,6 +6,7 @@ public class EmployeeResponse {
     private String name;
     private String email;
     private String bloodGroup;
+    private AddressResponse addressResponse;
 
     public int getId() {
         return id;
@@ -38,5 +39,14 @@ public class EmployeeResponse {
     public void setBloodGroup(String bloodGroup) {
         this.bloodGroup = bloodGroup;
     }
+
+    public AddressResponse getAddressResponse() {
+        return addressResponse;
+    }
+
+    public void setAddressResponse(AddressResponse addressResponse) {
+        this.addressResponse = addressResponse;
+    }
+
 
 }
--- src/main/java/dev/playground/service/EmployeeService.java
+++ src/main/java/dev/playground/service/EmployeeService.java
@@ -1,27 +1,42 @@
 package dev.playground.service;
 
 import dev.playground.entity.Employee;
+import dev.playground.model.AddressResponse;
 import dev.playground.model.EmployeeResponse;
 import dev.playground.repository.EmployeeRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmployeeService {
 
+    @Value("${address-service.base.url}")
+    private String addressBaseUrl;
+
     private final EmployeeRepository employeeRepository;
     private final ModelMapper modelMapper;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository, ModelMapper modelMapper) {
+    public EmployeeService(EmployeeRepository employeeRepository, ModelMapper modelMapper, RestTemplate restTemplate) {
         this.employeeRepository = employeeRepository;
         this.modelMapper = modelMapper;
+        this.restTemplate = restTemplate;
     }
 
     public EmployeeResponse getEmployeeById(int id) {
         Employee employee = employeeRepository.findById(id).orElse(null);
-        return modelMapper.map(employee, EmployeeResponse.class);
+        EmployeeResponse employeeResponse = modelMapper.map(employee, EmployeeResponse.class);
+
+        AddressResponse addressResponse = restTemplate.getForObject(
+                addressBaseUrl + "/address/{id}", AddressResponse.class, id
+        );
+        employeeResponse.setAddressResponse(addressResponse);
+
+        return employeeResponse;
     }
 
 }
