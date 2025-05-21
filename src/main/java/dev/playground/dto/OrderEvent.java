0eedf868848ade2471cf53d895f540279d992b0c
_hayas <hajcus10@seznam.cz>
2025-05-21 15:59:02 +0200

    [STOCK] - creating stock service (consumer service)

---
+++ src/main/java/dev/playground/dto/OrderEvent.java
@@ -0,0 +1,16 @@
+package dev.playground.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEvent {
+
+    private String message;
+    private String status;
+    private Order order;
+
+}
